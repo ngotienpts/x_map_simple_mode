77956ae419e5b12f72bf792569e50e6db946247a
Tien <ngotienpts@gmail.com>
2025-11-28 16:18:26 +0700

    /

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const lanes = document.querySelectorAll('.js__lane');
-    const maxSkew = 20;
+    const maxSkew = 30;
     const numLanes = lanes.length;
 
     lanes.forEach((lane, index) => {
         const normalizedIndex = (index / (numLanes - 1)) * 2 - 1;
         const skewValue = normalizedIndex * maxSkew;
-        lane.style.transform = `skewX(${skewValue}deg)`;
+        lane.style.transform = `skewX(${skewValue}deg) rotateX(80deg) scaleY(2)`;
 
         if (numLanes === 3 && index === 1) {
             lane.style.background = '#EEB669';
